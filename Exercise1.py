024faaba5694217311808bbb0fc3b47d3237e259
issabel.iuresi <issabel.iuresi@3pillarglobal.com>
2018-04-13 13:45:06 +0300

    -some text indentation added

--- Exercise1.py
+++ Exercise1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Exercise 1
 
-This exercise assumes that you have access to a copy of /etc/passwd, the file in which basic user information is stored on Unix computers.
-If you don't, then you can likely find such a file by searching for "/etc/passwd example" on the Web. The format is:
+This exercise assumes that you have access to a copy of /etc/passwd,
+the file in which basic user information is stored on Unix computers.
+If you don't, then you can likely find such a file by searching for "/etc/passwd example" on the Web.
+The format is:
 
 nobody:*:-2:-2::0:0:Unprivileged User:/var/empty:/usr/bin/false
 root:*:0:0::0:0:System Administrator:/var/root:/bin/sh
@@ -13,9 +15,11 @@ The first field is the username, and the third field is the ID of the user.
 The nobody user has ID -2, the root user has ID 0, and the daemon user has ID 1.
 You can ignore all but the first and third fields in the file.
 
-There is one exception to this format: A line that begins with a # character is a comment, and should be ignored by the parser.
+There is one exception to this format:
+    A line that begins with a # character is a comment, and should be ignored by the parser.
 
-For this exercise, you must create a dictionary based on /etc/passwd, in which the dict's keys are usernames and the values are the numeric IDs of those users.
+For this exercise, you must create a dictionary based on /etc/passwd,
+in which the dict's keys are usernames and the values are the numeric IDs of those users.
 You should then iterate through this dict, displaying one username and user ID on each line in alphabetical order.
 
 """
